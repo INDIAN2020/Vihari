3e416b1b4cfe834d3a35aa0b8f33f626fa686f0f
INDIAN2020 <gogula.sivannarayana@gmail.com>
2016-01-02 06:49:26 +0530

    New features added

---
+++ lib/admintheme.php
@@ -0,0 +1,8 @@
+<?php 
+function material_style() {
+    wp_enqueue_style('material-theme', plugins_url('../assets/material.css', __FILE__));
+     wp_enqueue_style('material-raleway', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,700');
+}
+add_action('admin_enqueue_scripts', 'material_style');
+add_action('login_enqueue_scripts', 'material_style');
+ ?>
